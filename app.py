84fa61d7711e64e591fdbc7349324e7f83618993
DobrevaI <iidobreva24a@gmail.com>
2026-04-14 14:22:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,2 @@
 import streamlit as st
-bad_ingredients = [""
+bad_ingredients = ["E110", "palm oil", "E102", "E132", "hydrogenated oils", "high-fructose corn syrup", "sodium nitrite", "potassium bromate", "E151", "E154", "E210", "E296", "E310", "E320", "E322", "E553b"]
