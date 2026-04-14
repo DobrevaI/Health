aaedc28347e73906a4831b98fc529e710e5f5689
DobrevaI <iidobreva24a@gmail.com>
2026-04-14 14:28:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,2 @@
 import streamlit as st
-bad_ingredients = ["E110", "palm oil", "E102", "E132", "hydrogenated oils", "high-fructose corn syrup", "sodium nitrite", "potassium bromate", "E151", "E154", "E210", "E296", "E310", "E320", "E322", "E553b"]
+bad_ingredients = ["E110" "Sunset Yellow FCF", "palm oil", "E102", "Tartrazine", "E132" "Indigotine", "Indigo Carmine", "hydrogenated oils", "high-fructose corn syrup", "sodium nitrite", "potassium bromate", "E151", "Black PN", "E154", "Brown FK", "E210", "Benzoic acid", "E296", "Malic Acid", "E310", "Propyl Gallate", "E320", "E322", "Lecithin", "E553b", "Talc", "Talcum"]
